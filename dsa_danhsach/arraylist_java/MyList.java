483cd1098d693388f8080acb8a146cc32fff9ec4
Phuctam03 <hophuctam7112003@gmail.com>
2023-04-18 17:26:33 +0700

    suabaitap

--- dsa_danhsach/arraylist_java/MyList.java
+++ dsa_danhsach/arraylist_java/MyList.java
@@ -5,16 +5,15 @@ import java.util.Arrays;
 
 
 public class MyList<E> {
-    private ArrayList<E> mylist = new ArrayList<E>();
     private int size = 0;
     private static final int DEFAULT_CAPACITY = 10;
     private Object elements[];
 
     public MyList() {
+        elements = new Object[DEFAULT_CAPACITY];
     }
 
-    public MyList(ArrayList<E> mylist, int size, Object[] elements) {
-        this.mylist = mylist;
+    public MyList(int size, Object[] elements) {
         this.size = size;
         this.elements = elements;
     }
@@ -32,6 +31,7 @@ public class MyList<E> {
     }
 
     Object[] getElements() {
+        elements = new Object[DEFAULT_CAPACITY];
         return elements;
     }
 
@@ -40,46 +40,101 @@ public class MyList<E> {
     }
 
     void addMyList(int index, E element) {
-        mylist.add(index, element);
-
+        elements = new Object[DEFAULT_CAPACITY];
+        if (index < 0 || index > elements.length) {
+            throw new IndexOutOfBoundsException(index);
+        } else {
+            size++;
+            elements = new Object[elements.length + size];
+            for (int i = 0; i < elements.length; i++) {
+                if (i == index) {
+                    elements[index] = element;
+                    break;
+                }
+            }
+            for (int i = index + 1; i < elements.length; i++) {
+                elements[i] = elements[i - 1];
+            }
+        }
     }
 
     E removeMyList(int index) {
-        return mylist.remove(index);
+        int i, j;
+        if (index < 0 || index > elements.length) {
+            throw new IndexOutOfBoundsException(index);
+        } else {
+            for (i = 0; i < elements.length; i++) {
+                if (i == index) {
+                    for (j = index; j < elements.length - 1; j++) {
+                        elements[j] = elements[j - 1];
+                    }
+                }
+            }
+        }
+        return (E) elements[index];
     }
 
     int sizeMyList() {
-        return mylist.size();
+        return getSize();
     }
 
     E cloneMyList() {
-        return (E) mylist.clone();
+        Object newarrr[] = new Object[elements.length];
+        int index = 0;
+        for (Object object : elements) {
+            newarrr[index] = object;
+            index++;
+        }
+        return (E) newarrr;
+
     }
 
     boolean containsMyList(E o) {
-        return mylist.contains(o);
+        for (Object object : elements) {
+            if (object.equals(o)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     int indexofMyList(E o) {
-        return mylist.indexOf(o);
+        int index = 0;
+        for (int i = 0; i < elements.length; i++) {
+            if (elements[i].equals(o)) {
+                index = i;
+                break;
+            }
+        }
+        return index;
     }
 
-    boolean addList(E o) {
-        return mylist.add(o);
+    // void ensureCapacity(int minCapacity) {
+
+    // }
+
+    void ensureCapa() {
+        int newSize = elements.length * 2;
+        elements = Arrays.copyOf(elements, newSize);
     }
 
-    void ensureCapacity(int minCapacity) {
-        minCapacity = elements.length / 2;
-        elements = Arrays.copyOf(elements, minCapacity);
+    void add(E o) {
+        if (this.size == elements.length) {
+            ensureCapa();
+        }
+        elements[size++] = o;
     }
 
     E getMyList(int i) {
-        return mylist.get(i);
+        if (i >= size || i < 0) {
+            throw new IndexOutOfBoundsException("Index" + i + ", Size" + i);
+        }
+        return (E) elements[i];
     }
 
     void clearMyList() {
-        mylist.clear();
-
+        for (int i = 0; i < elements.length; i++) {
+            elements[i] = null;
+        }
     }
-
 }
--- dsa_danhsach/arraylist_java/MyListTest.java
+++ dsa_danhsach/arraylist_java/MyListTest.java
@@ -3,38 +3,16 @@ package dsa_danhsach.arraylist_java;
 public class MyListTest {
     public static void main(String[] args) {
         MyList<String> mylist = new MyList<String>();
+        MyList<String> a = new MyList<>();
 
-        mylist.addMyList(0, "A");
-        mylist.addMyList(1, "B");
-        mylist.addMyList(2, "C");
-        mylist.addMyList(3, "D");
-        mylist.addMyList(4, "E");
-        mylist.addMyList(5, "F");
+        a.add("A");
+        a.add("B");
+        a.add("C");
+        a.add("D");
+        System.out.println("phần tử được lấy ra theo index :" + a.getMyList(0));
+        System.out.println("Phần tử xóa và trả về theo vị trí" + a.removeMyList(4));
 
-        System.out.println("elements 1 :" + mylist.getMyList(0));
-        System.out.println("elements 2 :" + mylist.getMyList(1));
-        System.out.println("elements 3 :" + mylist.getMyList(2));
-        System.out.println("elements 4 :" + mylist.getMyList(3));
-        System.out.println("elements 5 :" + mylist.getMyList(4));
-        System.out.println("elements 6 :" + mylist.getMyList(5));
-
-        System.out.println("Amount is :" + mylist.sizeMyList());
-
-        System.out.println("Remove elements 3 is :" + mylist.removeMyList(2));
-
-        System.out.println("The Test ELement 3 in myList is :" + mylist.containsMyList("A"));
-        System.out.println("The Test ELement 3 in myList is :" + mylist.containsMyList("C"));
-
-        System.out.println("Index of Elements D in mylist is :" + mylist.indexofMyList("D"));
-
-        System.out.println("Test :" + mylist.addList("G"));
-
-        // String newArray = mylist.cloneMyList();
-        // for (int i = 0; i < newArray.length(); i++) {
-        //     System.out.println(newArray.charAt(i) + " ");
-
-        // }
-        mylist.clearMyList();
+        System.out.println(a.containsMyList("A"));
 
     }
 }
